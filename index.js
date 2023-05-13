4db918deccc957911e7d8a468deedfac0f16cc2a
azan9292 <webasoma@gmail.com>
2023-05-13 23:03:40 +0700

    update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const config = new Configuration({
 
 const openai = new OpenAIApi(config);
 
-app.use(express.static(path.join(__dirname+"/public")));
+app.use(express.static(path.join(__dirname+"/docs")));
 app.use(bodyParser.json());
 app.use(cors());
 
